79ab53165e6ac35b882e4570f20ae51c05d5ec7c
lautarochapa <lautarochapa@gmail.com>
2022-02-13 17:06:52 -0300

    cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                   
-                                LAUTAROOOOO v2
+                                LAUTAROOOOO v3
                                 
                                 Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
